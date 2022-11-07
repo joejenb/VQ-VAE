d815908725c9131c28475321dcb10dfe98a63cbd
Joe <joejbailey@icloud.com>
2022-11-07 05:43:44 +0000

    Implemented simpel VAE fro CelebA dataset

---
+++ VAE.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from Residual import ResidualStack
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Encoder, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens//2,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
+                                 out_channels=num_hiddens,
+                                 kernel_size=4,
+                                 stride=2, padding=1)
+        self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1, padding=1)
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        x = F.relu(x)
+        
+        x = self._conv_2(x)
+        x = F.relu(x)
+        
+        x = self._conv_3(x)
+        return self._residual_stack(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, out_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Decoder, self).__init__()
+        
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3, 
+                                 stride=1, padding=1)
+        
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+        
+        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens, 
+                                                out_channels=num_hiddens//2,
+                                                kernel_size=4, 
+                                                stride=2, padding=1)
+        
+        self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens//2, 
+                                                out_channels=out_channels,
+                                                kernel_size=4, 
+                                                stride=2, padding=1)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        
+        x = self._residual_stack(x)
+        
+        x = self._conv_trans_1(x)
+        x = F.relu(x)
+        
+        return self._conv_trans_2(x)
+
+
+class VAE(nn.Module):
+    def __init__(self, config, device):
+        super(VAE, self).__init__()
+
+        self.device = device
+
+        self._embedding_dim = config.embedding_dim
+
+        self._encoder = Encoder(config.num_channels, config.num_hiddens,
+                                config.num_residual_layers, 
+                                config.num_residual_hiddens)
+
+        self._pre_sample = nn.Conv2d(in_channels=config.num_hiddens, 
+                                      out_channels=config.num_filters,
+                                      kernel_size=1, 
+                                      stride=1)
+
+        self.mu = nn.Linear(config.embedding_dim * 64, config.embedding_dim * 64)
+        self.log_var = nn.Linear(config.embedding_dim * 64, config.embedding_dim * 64)
+
+        self.pre_decode = nn.Linear(config.embedding_dim * 64, config.embedding_dim * 64)
+
+        self._decoder = Decoder(config.num_filters,
+                            config.num_channels,
+                            config.num_hiddens, 
+                            config.num_residual_layers, 
+                            config.num_residual_hiddens
+                        )
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
+
+    def interpolate(self, x, y):
+        if (x.size() == y.size()):
+            zx = self._encoder(x)
+            zx = self._pre_vq_conv(zx)
+
+            zy = self._encoder(y)
+            zy = self._pre_vq_conv(zy)
+
+            z = (zx + zy) / 2
+
+            z_shape = z.shape
+            flat_z = z.view(z_shape[0], z_shape[1], self._embedding_dim)
+
+            flat_z_quantised = self._hopfield(flat_z)#flat_z)
+
+            z_quantised = flat_z_quantised.view(z_shape)
+
+            xy_recon = self._decoder(z_quantised)
+
+            return xy_recon
+        return x
+
+    def forward(self, x):
+        z = self._encoder(x)
+        z = F.relu(self._pre_vq_conv(z))
+
+        z_shape = z.shape
+
+        flat_z = z.view(z_shape[0], -1)
+
+        mu = self.mu(flat_z)
+        log_var = self.var(flat_z)
+        z_sampled = self.reparameterize(mu, log_var)
+
+        flat_z_sampled = self.pre_decode(z_sampled)
+
+        z_sampled = flat_z_sampled.view(z_shape)
+
+        x_recon = self._decoder(z_sampled)
+
+        return x_recon, mu, log_var
---
+++ main.py
@@ -0,0 +1,153 @@
+from matplotlib.transforms import Transform
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+
+import wandb
+
+import torchvision
+from torchvision import transforms
+
+from VAE import VAE
+
+
+wandb.init(project="VAE")
+wandb.watch_called = False # Re-run the model without restarting the runtime, unnecessary after our next release
+
+# WandB – Config is a variable that holds and saves hyperparameters and inputs
+config = wandb.config          # Initialize config
+config.batch_size = 256          # input batch size for training (default: 64)
+config.epochs = 50             # number of epochs to train (default: 10)
+config.no_cuda = False         # disables CUDA training
+config.seed = 42               # random seed (default: 42)
+config.image_size = 96
+config.log_interval = 1     # how many batches to wait before logging training status
+config.learning_rate = 1e-3
+config.momentum = 0.1
+
+config.num_hiddens = 128
+config.num_residual_layers = 2
+config.num_residual_hiddens = 32
+config.num_embeddings = 512
+config.num_filters = 64
+config.embedding_dim = config.num_filters
+config.num_channels = 3
+config.data_set = "CELEBA"
+
+def get_data_loaders():
+    if config.data_set == "MNIST":
+        transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize(config.image_size),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])
+
+        train_set = torchvision.datasets.MNIST(root="/MNIST/", train=True, download=True, transform=transform)
+        val_set = torchvision.datasets.MNIST(root="/MNIST/", train=False, download=True, transform=transform)
+        test_set = torchvision.datasets.MNIST(root="/MNIST/", train=False, download=True, transform=transform)
+        num_classes = 10
+        config.data_variance = 1
+
+    elif config.data_set == "CIFAR10":
+        transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize(config.image_size),
+                transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+            ])
+        train_set = torchvision.datasets.CIFAR10(root="/CIFAR10/", train=True, download=True, transform=transform)
+        val_set = torchvision.datasets.CIFAR10(root="/CIFAR10/", train=False, download=True, transform=transform)
+        test_set = torchvision.datasets.CIFAR10(root="/CIFAR10/", train=False, download=True, transform=transform)
+        num_classes = 10
+        config.data_variance = np.var(train_set.data / 255.0)
+
+    elif config.data_set == "CELEBA":
+        transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize(config.image_size),
+                transforms.Normalize((0.5,0.5,0.5), (1.0,1.0,1.0))
+            ])
+
+        train_set = torchvision.datasets.CelebA(root="/CELEBA/", split='train', transform=transform, download=True)
+        val_set = torchvision.datasets.CelebA(root="/CELEBA/", split='test', transform=transform, download=True)
+        test_set = torchvision.datasets.CelebA(root="/CELEBA/", split='test', transform=transform, download=True)
+        num_classes = 0
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=config.batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=config.batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=config.batch_size, shuffle=False)
+    
+    return train_loader, val_loader, test_loader, num_classes
+
+
+def train(model, train_loader, optimiser):
+
+    model.train()
+    train_res_recon_error = 0
+
+    for X, _ in train_loader:
+        X = X.to(model.device)
+        optimiser.zero_grad()
+
+        X_recon = model(X)
+        recon_error = F.mse_loss(X_recon, X) / config.data_variance
+        recon_error.backward()
+
+        optimiser.step()
+        
+        train_res_recon_error += recon_error.item()
+
+    wandb.log({
+        "Train Reconstruction Error": train_res_recon_error / len(train_loader.dataset)
+    })
+
+
+def test(model, test_loader):
+    # Recall Memory
+    model.eval() 
+
+    test_res_recon_error = 0
+
+    example_images = []
+    example_reconstructions = []
+
+    with torch.no_grad():
+        for X, _ in test_loader:
+            X = X.to(model.device)
+
+            X_recon = model(X)
+            recon_error = F.mse_loss(X_recon, X) / config.data_variance
+            
+            test_res_recon_error += recon_error.item()
+
+        example_images = [wandb.Image(img) for img in X]
+        example_reconstructions = [wandb.Image(recon_img) for recon_img in X_recon]
+
+    wandb.log({
+        "Test Inputs": example_images,
+        "Test Reconstruction": example_reconstructions,
+        "Test Reconstruction Error": test_res_recon_error / len(test_loader.dataset)
+        })
+
+
+def main():
+
+    use_cuda = not config.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    train_loader, val_loader, test_loader, num_classes = get_data_loaders()
+
+    ### Add in correct parameters
+    model = VAE(config, device).to(device)
+    optimiser = optim.Adam(model.parameters(), lr=config.learning_rate, amsgrad=False)
+
+    wandb.watch(model, log="all")
+
+    for _ in range(config.epochs):
+        train(model, train_loader, optimiser)
+        test(model, test_loader)
+
+if __name__ == '__main__':
+    main()
