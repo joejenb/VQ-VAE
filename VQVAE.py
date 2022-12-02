ba21be2a1edeb428cf5d131e8ef584297d867c93
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-02 23:01:42 +0000

    Fixed bug with PixelCNN import

--- VQVAE.py
+++ VQVAE.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from PixelCNN import PixelCNN
+from PixelCNN.PixelCNN import PixelCNN
 
 class Encoder(nn.Module):
     def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
