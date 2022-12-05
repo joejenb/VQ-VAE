8fd88479815c7603641ff5e70694f4b86ea145c4
Joe Jenner-Bailey <joe_jenner_bailey@Joes-Mac-mini.home>
2022-12-05 11:07:39 +0000

    Reduced image size

--- configs/ffhq_64_config.py
+++ configs/ffhq_64_config.py
@@ -3,7 +3,7 @@ config["batch_size"] = 2048          # input batch size for training (default: 6
 config["epochs"] = 100             # number of epochs to train (default: 10)
 config["no_cuda"] = False         # disables CUDA training
 config["seed"] = 1265
-config["image_size"] = 64
+config["image_size"] = 32
 config["log_interval"] = 1     # how many batches to wait before logging training status
 config["learning_rate"] = 1e-3
 config["momentum"] = 0.1
@@ -18,7 +18,7 @@ config["embedding_dim"] = 32
 config["num_embeddings"] = 512
 config["num_channels"] = 3
 config["data_set"] = "FFHQ"
-config["representation_dim"] = 8
+config["representation_dim"] = 4
 config["commitment_cost"] = 0.25
 config["decay"] = 0.99
 
